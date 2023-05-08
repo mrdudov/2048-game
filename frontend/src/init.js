21c06022064c0590af9ccad2ed04ec941c6dcdc7
mrdudov <mrdudov@gmail.com>
2023-05-08 17:14:04 +0300

    random init game board

--- frontend/src/init.js
+++ frontend/src/init.js
@@ -1,12 +1,24 @@
 export { init }
 
+
 const game_board = [
-    [0, 2, 0, 0],
-    [0, 0, 2, 0],
-    [0, 0, 4, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
     [0, 0, 0, 0],
 ]
 
 function init() {
+    for (let i=0; i < 3; i++) {
+        let line_no = getRandomInt(0, 4)
+        let cell_no = getRandomInt(0, 4)
+        game_board[line_no][cell_no] = 2
+    }
     return game_board
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.ceil(max);
+    return Math.floor(Math.random() * (max - min) + min);
 }
