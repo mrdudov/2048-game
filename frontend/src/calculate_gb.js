14819018b12e8c041b35fb6f0ef1e348cd6b3ed9
mrdudov <mrdudov@gmail.com>
2023-05-09 17:45:12 +0300

    move up

--- frontend/src/calculate_gb.js
+++ frontend/src/calculate_gb.js
@@ -60,6 +60,24 @@ function left_move(board) {
 }
 
 function up_move(board) {
+    for (let cell_no = 0 ; cell_no < board.length; cell_no++) {
+        for (let line_no = 1; line_no < board.length; line_no++) {
+            if (board[line_no][cell_no] !== 0) {
+                let t = line_no
+                while (board[t-1] !== undefined) {
+                    if (board[t-1][cell_no] === 0) {
+                        board[t-1][cell_no] = board[t][cell_no]
+                        board[t][cell_no] = 0
+                    } else if(board[t][cell_no] === board[t-1][cell_no]) {
+                        board[t-1][cell_no] = 2 * board[t][cell_no]
+                        board[t][cell_no] = 0
+                        break
+                    }
+                    t -= 1
+                } 
+            }
+        }
+    }
     return board
 }
 
