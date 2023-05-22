5bc274bd0567eaca3c055abcf2fee6eae20d71df
mrdudov <mrdudov@gmail.com>
2023-05-22 22:47:14 +0300

    win component WIP

--- frontend/src/libs/win/win.js
+++ frontend/src/libs/win/win.js
@@ -1,6 +1,11 @@
-export { is_win }
+export { is_win, win_card }
 
-import { WIN_SCORE } from "../configure.js"
+import { WIN_SCORE } from "../../configure.js"
+import win_card_html from "./win.html"
+import "./win.css"
+
+const win_card = document.createElement('div')
+win_card.innerHTML = win_card_html
 
 function is_win(board) {
   for (let line_no = 0; line_no < board.length; line_no++) {
